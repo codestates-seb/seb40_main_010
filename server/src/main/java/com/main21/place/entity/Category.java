9f01af41b7049140e06a9d141f97855296ccee0d
LimJaeminZ <zentlemin@gmail.com>
2022-11-11 11:36:33 +0900

    ✨ feat: Category Entity 작성

--- server/src/main/java/com/main21/place/entity/Category.java
+++ server/src/main/java/com/main21/place/entity/Category.java
@@ -1,4 +1,23 @@
 package com.main21.place.entity;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CATEGORY_ID")
+    private Long id;
+
+    private String categoryName;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<PlaceCategory> placeCategories = new ArrayList<>();
 }
