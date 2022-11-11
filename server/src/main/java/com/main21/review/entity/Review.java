b5cae5b5d23872482149a6e7dbab46a57f8fa872
mozzi327 <mozzi327@gmail.com>
2022-11-11 15:21:22 +0900

    ✨ feat : 리뷰, 북마크 엔티티 추가

--- server/src/main/java/com/main21/review/entity/Review.java
+++ server/src/main/java/com/main21/review/entity/Review.java
@@ -1,4 +1,38 @@
 package com.main21.review.entity;
 
-public class Review {
+
+import com.main21.member.entity.Member;
+import com.main21.util.Auditable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review extends Auditable {
+
+    @Id
+    @Column(name = "REVIEW_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double score;
+
+    @Column(length = 1000)
+    private String comment;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @Builder
+    public Review(double score, String comment) {
+        this.score = score;
+        this.comment = comment;
+    }
 }
