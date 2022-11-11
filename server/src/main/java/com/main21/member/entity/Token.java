1fec6a35858802ebc3bda1bb8739fa3bd0cbcce1
mozzi327 <mozzi327@gmail.com>
2022-11-11 13:35:29 +0900

    ✨ feat : 토큰 엔티티 추가

--- server/src/main/java/com/main21/member/entity/Token.java
+++ server/src/main/java/com/main21/member/entity/Token.java
@@ -1,4 +1,34 @@
 package com.main21.member.entity;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Token {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "REFRESH_TOKEN_ID")
+    private Long id;
+    private String userEmail;
+    private String refreshToken;
+    private Long userId;
+
+    @Builder
+    public Token(String userEmail,
+                 String refreshToken,
+                 Long userId) {
+        this.userEmail = userEmail;
+        this.refreshToken = refreshToken;
+        this.userId = userId;
+    }
 }
