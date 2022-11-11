6effa925d84f70dc229b7ece4336554ea2611d5d
LimJaeminZ <zentlemin@gmail.com>
2022-11-11 15:45:05 +0900

    ✨ feat : Reserve Create 생성

--- server/src/main/java/com/main21/reserve/service/ReserveService.java
+++ server/src/main/java/com/main21/reserve/service/ReserveService.java
@@ -1,20 +1,35 @@
 package com.main21.reserve.service;
 
+import com.main21.exception.BusinessLogicException;
+import com.main21.exception.ExceptionCode;
+import com.main21.place.entity.Place;
+import com.main21.place.repository.PlaceRepository;
 import com.main21.reserve.dto.ReserveDto;
 import com.main21.reserve.entity.Reserve;
+import com.main21.reserve.repository.ReserveRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ReserveService {
-    //private final PlaceRepository placeRepository; 장소 리포지토리 생성 시 확인
+
+    private final ReserveRepository reserveRepository;
+    private final PlaceRepository placeRepository;
 
     public Reserve createReserve(ReserveDto.Post post, Long placeId) {
 
-        //Place findPlace = placeRepository.findPlaceById(placeId).orElseThrow()
+        //공간 확인
+        Place findPlace = placeRepository.findById(placeId).orElseThrow(() ->
+                new BusinessLogicException(ExceptionCode.PLACE_NOT_FOUND));
 
-        //Reserve reserve = new Reserve()
+        //유저확인
+
+        Reserve reserve = new Reserve(post.getCapacity(), post.getCheckIn(), post.getCheckOut());
+
+        //매핑 관계 저장
+
+        Reserve savedReserve = reserveRepository.save(reserve);
 
         return null;
     }
