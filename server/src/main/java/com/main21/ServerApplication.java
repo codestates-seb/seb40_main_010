211974d47e18c91d32e9bef8e46f2c7a11a78cdd
quartz614 <quartz614@naver.com>
2022-11-11 14:00:08 +0900

    ✨ feat : Member Entity 작성

--- server/src/main/java/com/main21/ServerApplication.java
+++ server/src/main/java/com/main21/ServerApplication.java
@@ -2,7 +2,9 @@ package com.main21;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ServerApplication {
 
