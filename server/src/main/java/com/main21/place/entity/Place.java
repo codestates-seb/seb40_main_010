b07cfb1b585f839b8d0761b7e2686a0d40b2adae
LeeGoh <gyuleegoh@gmail.com>
2022-11-11 11:28:35 +0900

    ✨ feat : Place entity 작성

--- server/src/main/java/com/main21/place/entity/Place.java
+++ server/src/main/java/com/main21/place/entity/Place.java
@@ -1,4 +1,49 @@
 package com.main21.place.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Place {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+
+    @Column(length = 10000)
+    private String detailInfo;
+
+    private int maxCapacity;
+
+    @Column(length = 500)
+    private String address;
+
+    private int charge;
+
+    private double score;
+
+    private int view;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    @OneToMany(mappedBy = "place")
+    private List<PlaceCategory> placeCategories = new ArrayList<>();
+
+    @OneToMany(mappedBy = "place")
+    private List<PlaceImage> placeImages = new ArrayList<>();
+
+    @OneToMany(mappedBy = "place")
+    private List<MBTICount> mbtiCounts = new ArrayList<>();
 }
