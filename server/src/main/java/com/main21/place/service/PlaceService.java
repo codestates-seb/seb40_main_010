65c559c97d868b76130bb64a67be18a0b4911364
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2022-11-11 15:32:03 +0900

    🐛 fix : place 로직 수정 (#19)
    🐛 fix : place 로직 수정 확인

--- server/src/main/java/com/main21/place/service/PlaceService.java
+++ server/src/main/java/com/main21/place/service/PlaceService.java
@@ -3,24 +3,23 @@ package com.main21.place.service;
 import com.main21.place.dto.PlaceDto;
 import com.main21.place.entity.Place;
 import com.main21.place.repository.PlaceRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PlaceService {
 
     private final PlaceRepository placeRepository;
-
-    public PlaceService(PlaceRepository placeRepository) {
-        this.placeRepository = placeRepository;
-    }
 
     /*
      * 공간 등록 메서드
      * @param place
      * @return
      */
-    public Place createPlace(Place place) {
-        return placeRepository.save(place);
+    public Place createPlace(PlaceDto.Post post) {
+        Place place = new Place(post.getTitle());
+        return place;
     }
 
 
