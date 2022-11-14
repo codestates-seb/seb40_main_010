36ffe0df77d98a6acdbca5adcb8d136a617cea31
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2022-11-15 01:42:01 +0900

    ♻️ refactor : @CreatedDate import 추가 (#59)

--- server/src/main/java/com/main21/reserve/dto/ReserveDto.java
+++ server/src/main/java/com/main21/reserve/dto/ReserveDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
