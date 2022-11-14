a17b20aea5a5023e6d872fe7db79658c145e0d55
LimJaeminZ <107789417+LimJaeminZ@users.noreply.github.com>
2022-11-14 13:56:43 +0900

    ✨ feat : PlaceImage 사진 업로드 (#42)

---
+++ server/src/main/java/com/main21/place/dto/PlacePostDto.java
@@ -0,0 +1,19 @@
+package com.main21.place.dto;
+
+import com.main21.place.entity.PlaceImage;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Builder
+@Getter
+public class PlacePostDto {
+    private String title;
+    private List<String> categoryList;
+    private int maxCapacity;
+    private String address;
+    private String detailInfo;
+    //private List<PlaceImage> placeImages;
+    private int charge;
+}
--- server/src/main/java/com/main21/place/service/PlaceService.java
+++ server/src/main/java/com/main21/place/service/PlaceService.java
@@ -1,34 +1,50 @@
 package com.main21.place.service;
 
-import com.main21.place.dto.PlaceDto;
+import com.main21.place.dto.PlacePostDto;
 import com.main21.place.entity.Place;
+import com.main21.place.entity.PlaceImage;
+import com.main21.place.repository.PlaceImageRepository;
 import com.main21.place.repository.PlaceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PlaceService {
 
     private final PlaceRepository placeRepository;
+    private final PlaceImageRepository placeImageRepository;
+    private final FileHandler fileHandler;
 
     /*
      * 공간 등록 메서드
      * @param place
      * @return
      */
-    public Place createPlace(PlaceDto.Post post) {
+    @Transactional
+    public Long create(PlacePostDto postDto, List<MultipartFile> files) throws Exception {
+        Place place = new Place(
+                postDto.getTitle(),
+                postDto.getDetailInfo(),
+                postDto.getMaxCapacity(),
+                postDto.getAddress(),
+                postDto.getCharge()
+        );
 
-        Place place = Place.builder()
-                .title(post.getTitle())
-                .detailInfo(post.getDetailInfo())
-                .maxCapacity(post.getMaxCapacity())
-                .address(post.getAddress())
-                .charge(post.getCharge())
-                .build();
+        List<PlaceImage> placeImageList = fileHandler.parseFileInfo(files);
 
-        return placeRepository.save(place);
+        //파일이 존재할 때 처리
+        if(!placeImageList.isEmpty()) {
+            for(PlaceImage placeImage : placeImageList) {
+                //파일 DB 저장
+                place.addPlaceImage(placeImageRepository.save(placeImage));
+            }
+        }
+
+        return placeRepository.save(place).getId();
     }
-
-
 }
