9f0ecb3dd38b69492cc066b8a10f4dd951c4009b
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2022-11-14 21:55:51 +0900

    ✨ feat : exception 코드 작성 (#52)

--- server/src/main/java/com/main21/reserve/dto/ReserveDto.java
+++ server/src/main/java/com/main21/reserve/dto/ReserveDto.java
@@ -1,9 +1,11 @@
 package com.main21.reserve.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -15,7 +17,11 @@ public class ReserveDto {
     public static class Post {
         private Long id;
         private int capacity;
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
         private Date startTime;
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
         private Date endTime;
     }
 }
