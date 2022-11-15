56fa177d7f0b9bb92d55a084d161586ba45c03c3
quartz614 <quartz614@naver.com>
2022-11-15 14:31:15 +0900

    ♻️ refactor : Review Service 수정

--- server/src/main/java/com/main21/review/repository/CustomReviewRepository.java
+++ server/src/main/java/com/main21/review/repository/CustomReviewRepository.java
@@ -1,6 +1,10 @@
 package com.main21.review.repository;
 
 
-public interface CustomReviewRepository {
+import com.main21.review.dto.ReviewDto;
 
+import java.util.List;
+
+public interface CustomReviewRepository {
+    List<ReviewDto.Response> getReviews(Long placeId);
 }
