ed9e2a432094939d2eda1e27ca797823f328518d
LeeGoh <gyuleegoh@gmail.com>
2022-11-15 17:19:35 +0900

    ✨ feat : 예약 조회 구현 및 예약 등록 수정

--- server/src/main/java/com/main21/reserve/repository/ReserveRepository.java
+++ server/src/main/java/com/main21/reserve/repository/ReserveRepository.java
@@ -3,5 +3,5 @@ package com.main21.reserve.repository;
 import com.main21.reserve.entity.Reserve;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReserveRepository extends JpaRepository<Reserve, Long> {
+public interface ReserveRepository extends JpaRepository<Reserve, Long>, CustomReserveRepository {
 }
