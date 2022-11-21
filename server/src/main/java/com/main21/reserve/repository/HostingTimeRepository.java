177c8821d00c3df420fead96f68e7835d972325d
LimJaeminZ <107789417+LimJaeminZ@users.noreply.github.com>
2022-11-21 21:54:32 +0900

    ✨ feat : 예약시간 생성 로직2 (#150)

--- server/src/main/java/com/main21/reserve/repository/HostingTimeRepository.java
+++ server/src/main/java/com/main21/reserve/repository/HostingTimeRepository.java
@@ -3,5 +3,8 @@ package com.main21.reserve.repository;
 import com.main21.reserve.entity.HostingTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HostingTimeRepository extends JpaRepository<HostingTime, Long> {
+    Optional<HostingTime> findByReserveDate(String reserveDate);
 }
--- server/src/main/java/com/main21/reserve/repository/TimeStatusRepository.java
+++ server/src/main/java/com/main21/reserve/repository/TimeStatusRepository.java
@@ -4,4 +4,6 @@ import com.main21.reserve.entity.TimeStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TimeStatusRepository extends JpaRepository<TimeStatus, Long> {
+
+    TimeStatus findByHostingTimeIdAndStartTime(Long HostingTimeId, Integer startTime);
 }
