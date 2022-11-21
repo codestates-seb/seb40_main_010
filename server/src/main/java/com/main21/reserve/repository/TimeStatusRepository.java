ca81437565cd981e30936f0388f43a58949eae53
Quartz614 <96826217+Quartz614@users.noreply.github.com>
2022-11-22 00:28:40 +0900

    ✨ feat : 예약 maxSpace 구현 (#155)

--- server/src/main/java/com/main21/reserve/repository/TimeStatusRepository.java
+++ server/src/main/java/com/main21/reserve/repository/TimeStatusRepository.java
@@ -3,6 +3,8 @@ package com.main21.reserve.repository;
 import com.main21.reserve.entity.TimeStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TimeStatusRepository extends JpaRepository<TimeStatus, Long> {
 
     TimeStatus findByHostingTimeIdAndStartTime(Long HostingTimeId, Integer startTime);
