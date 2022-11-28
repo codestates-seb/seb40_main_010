25743f187de164a2e0452432a4cbc48b5d96c50e
LimJaeminZ <107789417+LimJaeminZ@users.noreply.github.com>
2022-11-28 13:16:50 +0900

    ✨ feat : 유저 이미지 수정 (#257)

--- server/src/main/java/com/main21/member/repository/MemberImageRepository.java
+++ server/src/main/java/com/main21/member/repository/MemberImageRepository.java
@@ -4,4 +4,6 @@ import com.main21.member.entity.MemberImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberImageRepository extends JpaRepository<MemberImage, Long> {
+
+    void deleteByMemberId(Long memberId);
 }
