2a7ba6eca49af177a889256195018974a4a1a180
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2022-11-29 20:39:19 +0900

    ♻️ refactor: Place Test 코드 분리 (#272)
    Co-authored-by: LeeGoh <“gyuleegoh@gmail.com”>

---
+++ server/src/test/java/com/main21/place/controller/PlaceControllerPatchTest.java
@@ -0,0 +1,13 @@
+package com.main21.place.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PlaceControllerPatchTest extends PlaceControllerTest{
+
+    @Test
+    @DisplayName("PATCH 장소 수정")
+    void patchPlace() throws Exception{
+
+    }
+}
