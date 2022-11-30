1ca4b0b2c91ee6094381b7bfbf5281e50cd6efb3
LeeGoh <gyuleegoh@gmail.com>
2022-11-30 17:49:29 +0900

    ♻️ refactor: Reserve Test 기본 구조 작성

---
+++ server/src/test/java/com/main21/reserve/controller/ReserveGetTest.java
@@ -0,0 +1,14 @@
+package com.main21.reserve.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ReserveGetTest extends ReserveControllerTest{
+
+    @Test
+    @DisplayName("GET 예약 조회")
+    void getReservations() {
+
+    }
+
+}
