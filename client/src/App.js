d58053ac4faa6424144d94136fdaef60615180a5
ichenny <ichenny.lee@gmail.com>
2022-12-06 14:00:45 +0900

    카카오 oauth 로그인 구현

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,6 @@ import LogIn from './pages/LogIn';
 import MyPage from './pages/MyPage';
 import Register from './pages/Register';
 import SignUp from './pages/SignUp';
-import NotFound from './components/NotFound';
 import OAuth2RedirectHandler from './components/Login/OAuth2RedirectHandler';
 import { HasRefresh, mbtiPlaceDataState, userMbtiValue } from './atoms';
 
@@ -82,7 +81,6 @@ function App() {
         <Route path="/my-page" element={<MyPage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="*" element={<NotFound />} />
         <Route path="/oauth/kakao" element={<OAuth2RedirectHandler />} />
         <Route path="/oauth/google" element={<OAuth2RedirectHandler />} />
       </Routes>
