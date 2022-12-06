cbfcfa7e7b9ac5119e387d958015a72e386b689a
ichenny <ichenny.lee@gmail.com>
2022-12-06 21:30:22 +0900

    :bug: fix : 소셜로그인 mbti 반영

--- client/src/components/Login/OAuth2RedirectHandler.js
+++ client/src/components/Login/OAuth2RedirectHandler.js
@@ -2,12 +2,13 @@ import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
 
-import { HasRefresh } from '../../atoms';
+import { HasRefresh, userMbtiValue } from '../../atoms';
 import Loading from '../Loading';
 
 function OAuth2RedirectHandler() {
   const navigator = useNavigate();
   const setIsLogIn = useSetRecoilState(HasRefresh);
+  const setUserMbti = useSetRecoilState(userMbtiValue);
 
   const Access = new URL(window.location.href).searchParams.get(
     'authorization',
@@ -15,9 +16,15 @@ function OAuth2RedirectHandler() {
   const Refresh = new URL(window.location.href).searchParams.get(
     'refreshtoken',
   );
+
+  const Mbti = new URL(window.location.href).searchParams.get('MBTI');
+
+  console.log(Mbti);
+
   useEffect(() => {
     localStorage.setItem('ACCESS', Access);
     localStorage.setItem('REFRESH', Refresh);
+    setUserMbti(Mbti);
     setIsLogIn(true);
     navigator('/');
   }, []);
